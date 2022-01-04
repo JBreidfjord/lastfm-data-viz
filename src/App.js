1be469b815f60cb56d33ad765656fead7119fcb3
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-01-04 14:33:42 -0700

    Navbar component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Navbar from "./components/Navbar";
 import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
+        <div className="container">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
