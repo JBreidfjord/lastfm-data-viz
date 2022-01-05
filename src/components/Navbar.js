6ab15b024b94a2736648f22233a4c7d6ab55a0d0
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-01-04 17:31:12 -0700

    Conditional NavLinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
 import "./Navbar.css";
 
 import { NavLink } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Navbar() {
+  const { user } = useAuthContext();
+
   return (
     <div className="navbar">
       <ul>
@@ -11,15 +14,20 @@ export default function Navbar() {
             <span>Data Viz</span>
           </NavLink>
         </li>
-        <li>
-          <button className="btn">Logout</button>
-        </li>
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/signup">Signup</NavLink>
-        </li>
+        {user ? (
+          <li>
+            <button className="btn">Logout</button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+            <li>
+              <NavLink to="/signup">Signup</NavLink>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
