f77a7fa1951412c5a834f77af41a2bb187d73f95
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-01-05 11:57:39 -0700

    Input for user to fetch data for

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,32 @@
 import "./Home.css";
 
+import { useState } from "react";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [lastfmUsername, setLastfmUsername] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // TODO: check if user data is already in storage
+    // TODO: request data from last.fm API
+  };
+
+  return (
+    <div>
+      <h2>Data Viz</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Last.FM Username</span>
+          <input
+            type="text"
+            onChange={(e) => setLastfmUsername(e.target.value)}
+            value={lastfmUsername}
+            required
+          />
+        </label>
+        <button className="btn">Fetch Data</button>
+      </form>
+    </div>
+  );
 }
