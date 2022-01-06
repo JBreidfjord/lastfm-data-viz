98c4b914ed298e1728b5a4f8060766bbd9d52270
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-01-06 16:26:06 -0700

    Use github secrets instead of .env; .env.local for dev

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3Ax3FSIh6Jg_V9E-60-fSnRjwVrULk_Q",
-  authDomain: "lastfm-data-viz.firebaseapp.com",
-  projectId: "lastfm-data-viz",
-  storageBucket: "lastfm-data-viz.appspot.com",
-  messagingSenderId: "751652618078",
-  appId: "1:751652618078:web:c177271aaf078feb69d9cf",
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
