240b5a5c308739c04b88f796c3f6360328db3a28
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-01-06 12:11:33 -0700

    Refactor Home component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,129 +8,138 @@ import { useCSVStorage } from "../../hooks/useCSVStorage";
 import { useFetch } from "../../hooks/useFetch";
 import { useNavigate } from "react-router-dom";
 
+const initialInfoState = {
+  page: 0,
+  totalPages: 0,
+  totalTracks: 0,
+  user: "",
+  url: "",
+  fetchFromDate: 0,
+  fetching: false,
+  doneFetching: false,
+  success: false,
+};
+
 const infoReducer = (state, action) => {
   switch (action.type) {
     case "SET_USER":
-      return { ...state, user: action.payload, page: 0 };
-    case "SET_ALL":
+      return { ...initialInfoState, user: action.payload };
+    case "SET_INFO":
       return { ...state, ...action.payload };
+    case "SET_URL":
+      return { ...state, url: action.payload };
+    case "SET_URL_AND_DATE":
+      return { ...state, url: action.payload.url, fetchFromDate: action.payload.fetchFromDate };
+    case "START_FETCH":
+      return { ...state, fetching: true, doneFetching: false };
+    case "FINISH_FETCH":
+      return { ...state, fetching: false, doneFetching: true };
+    case "FINISH_UPLOAD":
+      return { ...state, success: true };
     default:
       return state;
   }
 };
 
 export default function Home({ setScrobbleData }) {
-  const {
-    upload,
-    download,
-    storageData,
-    error: storageError,
-    isPending: isStoragePending,
-  } = useCSVStorage();
-  const [isFetching, setIsFetching] = useState(false);
-  const [fetchFromDate, setFetchFromDate] = useState(null);
-  const [url, setUrl] = useState("");
-  const { data, error: fetchError } = useFetch(url);
-  const [infoState, dispatch] = useReducer(infoReducer, { page: 0, totalPages: 0, user: "" });
+  const { upload, error: storageError, isPending: isStoragePending } = useCSVStorage();
+  const [infoState, dispatch] = useReducer(infoReducer, initialInfoState);
+  const { data, error: fetchError } = useFetch(infoState.url);
   const [tracks, setTracks] = useState([]);
   const [time, setTime] = useState(null);
-  const [success, setSuccess] = useState(false);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     dispatch({ type: "SET_USER", payload: e.target.value });
     setTracks([]);
-    setSuccess(false);
-    setFetchFromDate(null);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    getDoc(doc(db, "scrobbles", infoState.user.toLowerCase()))
+      .then((doc) => {
+        const fetchFromDate = doc.exists() ? doc.data().lastUpdated.seconds : 0;
+        dispatch({
+          type: "SET_URL_AND_DATE",
+          payload: {
+            url:
+              `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks` +
+              `&user=${infoState.user}&api_key=${process.env.REACT_APP_API_KEY}` +
+              `&format=json&limit=200&from=${fetchFromDate}`,
+            fetchFromDate,
+          },
+        });
+        // TODO: Handle downloading data and setting tracks
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
     setTime(Timestamp.now());
-    setUrl(
-      `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${infoState.user}` +
-        `&api_key=${process.env.REACT_APP_API_KEY}&format=json&limit=200`
-    );
   };
 
-  // Handle setting tracks and infoState
+  useEffect(() => {
+    console.log(infoState);
+  }, [infoState]);
+
   useEffect(() => {
     if (data) {
       const { track: trackList, "@attr": attr } = data["recenttracks"];
       dispatch({
-        type: "SET_ALL",
+        type: "SET_INFO",
         payload: {
-          user: attr["user"],
           totalPages: parseInt(attr["totalPages"]),
           page: parseInt(attr["page"]),
+          totalTracks: parseInt(attr["total"]),
         },
       });
-      setTracks((prevTracks) => [
-        ...prevTracks,
-        ...trackList.map((track) => {
-          return {
-            artist: track.artist["#text"],
-            album: track.album["#text"],
-            title: track.name,
-            date: track.date["uts"],
-          };
-        }),
-      ]);
-    }
-  }, [data]);
-
-  // Handle updating url
-  useEffect(() => {
-    if (infoState.page === 1) {
-      // Check if data exists in storage
-      getDoc(doc(db, "scrobbles", infoState.user))
-        .then((doc) => {
-          download(infoState.user);
-          setFetchFromDate(doc.exists() ? doc.data().lastUpdated.seconds : 0);
-        })
-        .catch((err) => {
-          console.log(err);
+      if (trackList.length > 0) {
+        if (attr["page"] === "1") {
+          dispatch({ type: "START_FETCH" });
+        }
+        dispatch({
+          type: "SET_URL",
+          payload:
+            `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks` +
+            `&user=${infoState.user}&api_key=${process.env.REACT_APP_API_KEY}` +
+            `&format=json&limit=200&from=${infoState.fetchFromDate}&page=${1 + +attr["page"]}`,
         });
-    }
-    if (infoState.page !== 0 && infoState.page < infoState.totalPages && fetchFromDate !== null) {
-      // Prevents flickering when user's data is up to date
-      if (infoState.page === 2) {
-        setIsFetching(true);
+        setTracks((prevTracks) => [
+          ...prevTracks,
+          ...trackList.map((track) => {
+            return {
+              artist: track.artist["#text"],
+              album: track.album["#text"],
+              title: track.name,
+              date: track.date["uts"],
+            };
+          }),
+        ]);
+      } else {
+        dispatch({ type: "FINISH_FETCH" });
       }
-      setUrl(
-        `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${infoState.user}` +
-          `&api_key=${process.env.REACT_APP_API_KEY}&format=json&limit=200` +
-          `&page=${infoState.page + 1}&from=${fetchFromDate}`
-      );
     }
+  }, [data, infoState.user, infoState.fetchFromDate]);
 
-    // TODO: handle cancelling fetch
-  }, [infoState, fetchFromDate, download]);
-
-  //temp
   useEffect(() => {
-    if (storageData) {
-      console.log(storageData);
-    }
-  }, [storageData]);
-
-  // Handle adding data to storage
-  useEffect(() => {
-    if (infoState.page !== 0 && infoState.page === infoState.totalPages && isFetching) {
-      setIsFetching(false);
+    if (infoState.doneFetching && !infoState.success) {
       upload(tracks, {
-        id: infoState.user,
+        id: infoState.user.toLowerCase(),
         data: { lastUpdated: time, lastUsed: Timestamp.now() },
       });
       setScrobbleData({ scrobbles: tracks, user: infoState.user });
-      setSuccess(true);
-    } else if (infoState.page > infoState.totalPages) {
-      setIsFetching(false);
-      setScrobbleData({ scrobbles: tracks, user: infoState.user });
-      setSuccess(true);
+      dispatch({ type: "FINISH_UPLOAD" });
     }
-  }, [infoState, tracks, time, upload, isFetching, setScrobbleData]);
+  }, [
+    infoState.user,
+    infoState.doneFetching,
+    infoState.success,
+    tracks,
+    time,
+    setScrobbleData,
+    upload,
+  ]);
 
   return (
     <div className="home">
@@ -143,10 +152,10 @@ export default function Home({ setScrobbleData }) {
             onChange={(e) => handleChange(e)}
             value={infoState.user}
             required
-            disabled={isFetching}
+            disabled={infoState.fetching}
           />
         </label>
-        {isFetching ? (
+        {infoState.fetching ? (
           <button className="btn" disabled>
             Fetching...
           </button>
@@ -155,16 +164,20 @@ export default function Home({ setScrobbleData }) {
         )}
       </form>
 
-      {data && isFetching && (
+      {data && infoState.fetching && (
         <p>
-          Fetching data for {infoState.user}... Page {infoState.page}/{infoState.totalPages}
+          Fetching data for {infoState.user}... {tracks.length}/{infoState.totalTracks} (
+          {((tracks.length / infoState.totalTracks) * 100).toFixed(2)}%)
         </p>
       )}
 
       {fetchError && (
         <div className="error">
           <p>{fetchError}</p>
-          <button className="btn" onClick={() => setUrl((prevUrl) => prevUrl + "#")}>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "SET_URL", payload: infoState.url + "#" })}
+          >
             Retry
           </button>
         </div>
@@ -181,7 +194,7 @@ export default function Home({ setScrobbleData }) {
 
       {isStoragePending ? (
         <p>Uploading data...</p>
-      ) : success ? (
+      ) : infoState.success ? (
         <div className="success">
           <p>Data processed successfully</p>
           <button className="btn" onClick={() => navigate("/dashboard")}>
