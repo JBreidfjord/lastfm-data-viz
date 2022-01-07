09e388fca6ad5847908d049c51f49f26a801add2
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-01-06 19:06:33 -0700

    Move Firebase initialization to index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import React from "react";
 import ReactDOM from "react-dom";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
+};
+
+initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
