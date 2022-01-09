b4431281a2835031ea4ab80f5c6f4586f2a65683
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-01-08 22:02:43 -0700

    Add parent sizing and styles

--- src/pages/dashboard/PlotList.js
+++ src/pages/dashboard/PlotList.js
@@ -1,16 +1,18 @@
 import "./PlotList.css";
 
 import ArtistAlbumPie from "./plots/ArtistAlbumPie";
+import { ParentSize } from "@visx/responsive";
 
 export default function PlotList({ data }) {
-  const plots = [<ArtistAlbumPie data={data} />];
-
+  const plots = [
+    <ParentSize>
+      {({ width, height }) => <ArtistAlbumPie data={data} width={width} height={height} />}
+    </ParentSize>,
+  ];
   return (
     <div className="plot-list">
       {plots.map((plot, i) => (
-        <div key={i} className="plot">
-          {plot}
-        </div>
+        <div key={i}>{plot}</div>
       ))}
     </div>
   );
