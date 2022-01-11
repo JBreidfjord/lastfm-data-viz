9af9b17570de45352d5cd360295eda019b2548ef
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-01-11 15:29:43 -0700

    Component for displaying single plot

---
+++ src/pages/dashboard/Plot.js
@@ -0,0 +1,15 @@
+import Close from "../../assets/close_fullscreen.svg";
+
+export default function Plot({ children, setFocus }) {
+  return (
+    <div className="plot">
+      {children}
+      <img
+        src={Close}
+        onClick={() => setFocus(null)}
+        className="fullscreen-icon close"
+        alt="Close Fullscreen"
+      />
+    </div>
+  );
+}
